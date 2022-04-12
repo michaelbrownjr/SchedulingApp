62503db86ba385ad72c7afb2e1b320cbfd2d77a7
Michael Brown <mikeabrownjr+github@gmail.com>
2022-04-11 21:52:35 -0400

    Initial commit for project

---
+++ src/Controller/LoginPage.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+/*
+* LoginPageController
+*
+* @author Michael Brown
+* */
+public class LoginPage implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("I am initialized!");
+    }
+}
